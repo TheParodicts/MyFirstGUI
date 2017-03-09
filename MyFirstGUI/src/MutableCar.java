f95fb84356df993b4086a046555289ca0f39a4f2
Brian Rodriguez Badillo <brian.rodriguez17@upr.edu>
2017-03-09 15:41:59 -0400

    Bunch of changes

---
+++ MyFirstGUI/src/MutableCar.java
@@ -0,0 +1,120 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+public class MutableCar {
+static long count = 0;
+	
+	private double xPos;
+	private double yPos;
+	private Color carColor;
+	
+	private int carSpeed;
+	private int carDirection;
+	
+	private int carSpeedY;
+	private int carDirectionY;
+		
+	public double getxPos() {
+		return xPos;
+	}
+	public void setxPos(double xPos) {
+		this.xPos = xPos;
+	}
+	public double getyPos() {
+		return yPos;
+	}
+	public void setyPos(double yPos) {
+		this.yPos = yPos;
+	}
+	public int getCarSpeed() {
+		return carSpeed;
+	}
+	public void setCarSpeed(int carSpeed) {
+		this.carSpeed = carSpeed;
+	}
+	public int getCarDirection() {
+		return carDirection;
+	}
+	public void setCarDirection(int carDirection) {
+		this.carDirection = carDirection;
+	}
+	public MutableCar(double xPos,double yPos, Color carColor, int speed, int direction){
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.carColor = carColor;
+		this.carDirection = direction;
+		this.carSpeed = speed;
+		this.carDirectionY = 1;
+		this.carSpeedY = 40;
+	}
+	public MutableCar(double xPos,double yPos, Color carColor){
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.carColor = carColor;
+		this.carDirection = 1;
+		this.carSpeed = 10;
+		this.carDirectionY = 1;
+		this.carSpeedY = 40;
+	}
+	
+	public int getCarSpeedY() {
+		return carSpeedY;
+	}
+	public void setCarSpeedY(int carSpeedY) {
+		this.carSpeedY = carSpeedY;
+	}
+	public int getCarDirectionY() {
+		return carDirectionY;
+	}
+	public void setCarDirectionY(int carDirectionY) {
+		this.carDirectionY = carDirectionY;
+	}
+	public void setPosition(double xPos, double yPos){
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+	
+	public void move(int deltax, int deltay){
+		this.xPos = xPos + deltax;
+		this.yPos = yPos + deltay;
+		
+	}
+	
+	public void draw(Graphics g){
+		
+		
+		
+		Point2D.Double rearWindowStart = new Point2D.Double(xPos+10,yPos+10);
+		Point2D.Double rearWindowEnd = new Point2D.Double(xPos+20,yPos+0);
+		Line2D.Double rearWindow = new Line2D.Double(rearWindowStart, rearWindowEnd);
+		
+		Point2D.Double roofStart = new Point2D.Double(xPos+20,yPos+0);
+		Point2D.Double roofEnd = new Point2D.Double(xPos+40,yPos+0);
+		Line2D.Double roof = new Line2D.Double(roofStart, roofEnd);
+		
+		Point2D.Double frontWindowStart = new Point2D.Double(xPos+40,yPos+0);
+		Point2D.Double frontWindowEnd = new Point2D.Double(xPos+50,yPos+10);
+		Line2D.Double frontWindow = new Line2D.Double(frontWindowStart, frontWindowEnd);
+		
+		Rectangle2D.Double body = new Rectangle2D.Double(xPos+0,yPos+10, 60, 10);
+		
+		Ellipse2D.Double rearTire = new Ellipse2D.Double(xPos+10, yPos+20, 10, 10);
+		Ellipse2D.Double frontTire = new Ellipse2D.Double(xPos+40, yPos+20, 10, 10);
+		
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.BLACK);
+		g2.draw(rearWindow);
+		g2.draw(roof);
+		g2.draw(frontWindow);
+		g2.draw(rearTire);
+		g2.draw(frontTire);
+		g2.setColor(carColor);
+		g2.draw(body);
+		
+	}
+}
--- MyFirstGUI/src/MyComponent.java
+++ MyFirstGUI/src/MyComponent.java
@@ -5,6 +5,7 @@ import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.util.Random;
 
 import javax.swing.JComponent;
 
@@ -12,13 +13,45 @@ public class MyComponent extends JComponent {
 	
 	static int counter = 0;
 	
+	static MutableCar theCar = new MutableCar(0,0,Color.BLACK, 10, 1);
+	static	Random genRand = new Random();
+	
+	public boolean carCrashed(MutableCar c){
+		return (((c.getCarDirection() >0) && (c.getxPos()+60 >= this.getWidth()))
+				|| 
+				(c.getCarDirection() <=0 && c.getxPos() <= 0));
+	}
+
+	public boolean carReachedTopOrBottom(MutableCar c){
+		if (c.getCarDirectionY() > 0){
+			return(c.getyPos()+30 >= this.getHeight()-40);
+		}
+		else{
+			return (c.getyPos()<=0);
+		}
+	}
+	
 	public void paintComponent(Graphics g) {
 		
-	Car c = new Car(0,0, Color.RED);
-	Car c2 = new Car(0,40,Color.BLUE);
+	theCar.draw(g);
+	theCar.move(theCar.getCarSpeed()*theCar.getCarDirection(),0);
 	
-	c.draw(g);
-	c2.draw(g);
+	
+	if(this.carCrashed(theCar)){
+		theCar.setCarDirection(theCar.getCarDirection()*-1);
+		if (this.carReachedTopOrBottom(theCar)){
+			theCar.setCarDirectionY(theCar.getCarDirectionY()*-1);
+		}
+		int deltax = theCar.getCarDirection()*theCar.getCarSpeed();
+	int deltay = theCar.getCarDirectionY()*theCar.getCarSpeedY();
+	
+		theCar.move(deltax, deltay);
+	}
+	/*theCar.setPosition(0, 0);
+	theCar.draw(g);
+	
+	theCar.setPosition(0, 40);*/
+	
 		
 		//Rectangle r2 = new Rectangle(this.getWidth()-50,this.getHeight()-50,50,50);
 			//	g2.draw(r2);
